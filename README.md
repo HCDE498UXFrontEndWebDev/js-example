683b4c00a8a3dba868804e16da072124ef463736
Mario Paolini <trepaolini@gmail.com>
2019-02-03 19:09:37 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Intro to Javascript and Debugger Demo
+
+This demo will go over how to use the VS Code debugger and quickly utilize some of the basic functions of JS
---
+++ example.js
@@ -0,0 +1,23 @@
+// "use strict"; // Put at the top of every .js file you use if coding for es5, es6 always runs in strict mode
+
+// js has no “main” method, you can just start writing code
+
+// To use the VS code debugger, first click on the "pages" icon in the top left
+// Then click "open folder" and open the folder that this file is in
+
+// Now, we can run the .js file from within VS code. Click the "play" button on the top right of the screen and the code below will run
+
+let intro = "hello world";
+
+console.log(intro);
+
+// To use the debugger, go to the terminal on VS code on the bottom and type in "live-server" like we have in the normal terminal
+
+// Now that the live server is running, we can use the debugger: Place a breakpoint here by clicking to the left of the line numbers on the left
+
+// Click the icon on the left that looks like a bug and press the play button on the top to start
+
+// You can see whatever variables have been declared and other useful information in the debug column
+// Use the play on the top of this window to continue on in the program
+
+console.log("hello world again");
